a4222227549c3d1a3785ace6e6c73a7901124d47
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-11 18:18:52 -0500

    all inputs event handler functions as expected

--- src/components/CreateAPlayer.jsx
+++ src/components/CreateAPlayer.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 
 const CreateAPlayer = () => {
+  const [playerName, setPlayerName] = useState("");
+  const [breed, setBreed] = useState("");
+  const [status, setStatus] = useState("");
+  const [pictureUrl, setPictureUrl] = useState("");
+
   return (
     <section className="new-player">
       <h2>Create A New Player</h2>
       <form>
-        <input placeholder="puppy name" />
-        <input placeholder="puppy breed" />
-        <input placeholder="field or bench" />
-        <input placeholder="image url" />
+        <input placeholder="puppy name" onChange={(event) => {setPlayerName(event.target.value)}} value={playerName} />
+        <input placeholder="puppy breed" onChange={(event) => {setBreed(event.target.value)}} value={breed} />
+        <input placeholder="field or bench" onChange={(event) => {setStatus(event.target.value) }} value={status} />
+        <input placeholder="image url" onChange={(event) => {setPictureUrl(event.target.value)}} value={pictureUrl} />
         <button>Submit Player</button>
       </form>
     </section>
