eb03ea58032a47514f853f076a82f2bd7286a29b
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-11 10:55:21 -0500

    details route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-
+import { Routes, Route } from "react-router-dom";
 const App = () => {
  
 
   return (
     <>
-      <h1>Testing</h1>
+      <h1>Puppy Bowl I</h1>
+      <section id="routes">
+        <Routes>
+          <Route path="/details" element={<h2>testing</h2> } />
+        </Routes>
+      </section>
     </>
   )
 }
