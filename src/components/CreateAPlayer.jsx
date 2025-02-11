5d9b2a74ba7a8008f7142ff78b1a0058e1c3f28f
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-11 18:58:41 -0500

    new players are adding successfully

--- src/components/CreateAPlayer.jsx
+++ src/components/CreateAPlayer.jsx
@@ -6,10 +6,38 @@ const CreateAPlayer = () => {
   const [status, setStatus] = useState("");
   const [pictureUrl, setPictureUrl] = useState("");
 
+  const addPuppy = async(event) => {
+    event.preventDefault();
+    
+    try {
+      const response = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2501-FTB-ET-WEB-FT/players", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name: playerName,
+          breed: breed,
+          status: status,
+          imageUrl: pictureUrl
+        })
+      });
+
+      const puppy = await response.json();
+      setPlayerName("");
+      setBreed("");
+      setStatus("");
+      setPictureUrl("");
+
+    } catch(error) {
+      alert(error);
+    }
+  }
+
   return (
     <section className="new-player">
       <h2>Create A New Player</h2>
-      <form>
+      <form onSubmit={addPuppy}>
         <input placeholder="puppy name" onChange={(event) => {setPlayerName(event.target.value)}} value={playerName} />
         <input placeholder="puppy breed" onChange={(event) => {setBreed(event.target.value)}} value={breed} />
         <input placeholder="field or bench" onChange={(event) => {setStatus(event.target.value) }} value={status} />
