6b840febe7f26cd12e85cc596ccf083511242cdd
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-11 12:24:08 -0500

    home page renders all puppies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+
+import Home from "./components/Home";
+
 const App = () => {
  
 
   return (
     <>
-      <h1>Puppy Bowl I</h1>
-      <section id="routes">
+      <header>
+        <h1>Puppy Bowl I</h1>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+      </header>
+      <section>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/details" element={<h2>testing</h2> } />
         </Routes>
       </section>
