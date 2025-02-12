952c8baf9b18c2e8145c4903edf90b5432322074
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-11 19:45:54 -0500

    delete pup component functions as expected

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import PupDetails from "./components/Details";
 import CreateAPlayer from "./components/CreateAPlayer";
+import DeleteAPup from "./components/DeleteAPup";
 
 const App = () => {
  
@@ -14,6 +15,7 @@ const App = () => {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/create">Create A Player</Link>
+          <Link to="/delete">Delete a Bad Puppy</Link>
         </nav>
       </header>
       <div>
@@ -21,6 +23,7 @@ const App = () => {
           <Route path="/" element={ <Home /> } />
           <Route path="/details/:id" element={ <PupDetails/> } />
           <Route path="/create" element={ <CreateAPlayer />} />
+          <Route path="/delete" element={ <DeleteAPup /> } />
         </Routes>
       </div>
     </>
